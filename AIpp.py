c5601fd16071133ebd436a2aea95d966b58ee02f
winter16907-png <winter16907@gmail.com>
2026-04-07 19:03:56 +0800

    Update AIpp.py

--- AIpp.py
+++ AIpp.py
@@ -277,8 +277,8 @@ with col1:
     <style>
         .glass-container {
             background: rgba(255, 255, 255, 0.2); /* 背景半透明 */
-            backdrop-filter: blur(10000px);         /* 背景模糊 */
-            -webkit-backdrop-filter: blur(10000px); /* 兼容 Safari */
+            backdrop-filter: blur(1000px);         /* 背景模糊 */
+            -webkit-backdrop-filter: blur(1000px); /* 兼容 Safari */
             border-radius: 15px;                 /* 圓角 */
             border: 1px solid rgba(255, 255, 255, 0.3); /* 邊框線 */
             padding: 20px;
